f68f7558040cfa3d9414e4ac1a4ce521e5a62847
FlorentinoG9 <77408910+FlorentinoG9@users.noreply.github.com>
2025-03-30 17:05:42 +0200

    basic schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,21 @@
 import { sql } from 'drizzle-orm'
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { pgSchema, pgTable, text, uuid } from 'drizzle-orm/pg-core'
+
+const auth = pgSchema('auth')
+
+export const users = auth.table('users', (t) => ({
+  id: t.uuid('id').primaryKey().default(sql`gen_random_uuid()`),
+}))
 
 export const tenants = pgTable('tenants', {
   id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
   name: text('name'),
+})
+
+export const profiles = pgTable('profiles', {
+  id: uuid('id').primaryKey().default(sql`gen_random_uuid()`),
+  name: text('name'),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  sb_user: uuid('sb_user').references(() => users.id),
 })
